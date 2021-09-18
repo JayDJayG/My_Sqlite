baa97994e914edda00c3197eae4977eab1d8f139
JayDJayG <j.d.guzman.v@gmail.com>
2021-09-18 16:50:16 -0500

    First test implemented for class

--- test/MySqliteRequestTest.py
+++ test/MySqliteRequestTest.py
@@ -1,3 +1,6 @@
 import sys
-sys.path.insert(0, './my-sqlite/my-sqlite')
+sys.path.insert(0, '../my-sqlite/')
 from my_sqlite_request import MySqliteRequest
+
+test_class = MySqliteRequest()
+print(test_class)
