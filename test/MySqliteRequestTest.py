b49ef1d7b11b04c320b6d7fbaa3e4a682b159f0b
JayDJayG <j.d.guzman.v@gmail.com>
2021-09-22 09:48:00 -0500

    Added comment to test file

--- test/MySqliteRequestTest.py
+++ test/MySqliteRequestTest.py
@@ -2,5 +2,8 @@ import sys
 sys.path.insert(0, '../my-sqlite/')
 from my_sqlite_request import MySqliteRequest
 
+#Instance of MySqliteRequest class
 test_class = MySqliteRequest()
+
+#prints generic output of MySqLiteRequest class
 print(test_class)
