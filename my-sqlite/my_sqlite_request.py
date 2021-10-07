9168a97ca567e575561fcf21351cd910c9d7740e
JayDJayG <j.d.guzman.v@gmail.com>
2021-10-07 10:39:58 -0500

    quick modification of class after checking it would not interfere with the project requirements

--- my-sqlite/my_sqlite_request.py
+++ my-sqlite/my_sqlite_request.py
@@ -16,7 +16,7 @@ class MySqliteRequest:
         print(f"current state of query is {self.query_dictionary}")
         return
 
-    def from_(self, table_name):
+    def __from__(self, table_name):
         """
         from_ implements the sql FROM command, each request must have one.
         from_ will take a string(table_name) this is the name of the csv file to query.
@@ -37,7 +37,7 @@ class MySqliteRequest:
         else:
             print(self.path_message)
 
-    def select_(self, string_s):
+    def __select__(self, string_s):
         """
         The select_ method implements the sql SELECT command. 
         It takes as the parameter a string OR an array of strings.
@@ -63,7 +63,7 @@ class MySqliteRequest:
         else:
             print (self.from_message)
 
-    def where_(self, column_name, criteria):
+    def __where__(self, column_name, criteria):
         """
         The where_ method takes two arguments. column_name targets the column and
         criteria the condition to actuate by filtering the entries within run_dictionary.
@@ -75,37 +75,37 @@ class MySqliteRequest:
         else:
             print (self.from_message)
 
-    def join_(self, column_on_db_a, filename_db_b, column_on_db_b):
+    def __join__(self, other, column_on_db_a, filename_db_b, column_on_db_b):
         """
         The join_ method loads another filename_db
         and will join both database on an on column.
         """
 
-    def order_(self, order, column_name):
+    def __order__(self, order, column_name):
         """
         Order Implement an order method which will received two parameters, 
         order (:asc or :desc) and column_name. 
         It will sort depending on the order base on the column_name.
         """
 
-    def insert_(self, table_name):
+    def __insert__(self, table_name):
         """
         Insert Implement a method to insert which will receive a table name (filename).
         It will continue to build the request.
         """
-    def values_(self, data):
+    def __values__(self, data):
         """
         Values Implement a method to values which will receive data.
         (a hash of data on format (key => value)).
         It will continue to build the request. During the run() you do the insert.
         """
-    def update_(self, table_name):
+    def __update__(self, table_name):
         """
         Update Implement a method to update which will receive a table name (filename).
         It will continue to build the request.
         An update request might be associated with a where request
         """
-    def set_(self, data):
+    def __set__(self, data):
         """
         Set Implement a method to update which will receive data
         (a hash of data on format (key => value)). 
@@ -113,12 +113,12 @@ class MySqliteRequest:
         An update request might be associated with a where request.
         """
 
-    def delete_(self):
+    def __delete__(self):
         """
         Delete Implement a delete method. 
         It set the request to delete on all matching row. 
         It will continue to build the request. 
         An delete request might be associated with a where request.
         """
-    def run_(self):
+    def __run__(self):
         print("HELLO")
