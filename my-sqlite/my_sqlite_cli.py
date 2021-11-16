1c4cefc85cdee7feeb1e5851da33d0c5b801f8e1
mammadu <mammadudiallo@gmail.com>
2021-11-15 20:35:21 -0600

    Remove commented lines

--- my-sqlite/my_sqlite_cli.py
+++ my-sqlite/my_sqlite_cli.py
@@ -1,24 +1,10 @@
 from my_sqlite_request import MySqliteRequest
 
-# valid_commands = [
-#     'from',
-#     'select',
-#     'where',
-#     'join',
-#     'order',
-#     'insert',
-#     'values',
-#     'update',
-#     'set',
-#     'delete'
-#     ]
-
 def print_title():
     program_version = "0.1"
     program_date = "2021-XX-XX"
     title = " ".join(["MySQLite version", program_version, program_date])
     print(title)
-
 
 def print_prompt():
     prompt = "my_sqlite_cli>"
@@ -27,12 +13,9 @@ def print_prompt():
 
 def parse_prompt(user_input, request_object):
     tokens = user_input.split()
-    # print(tokens) #debug
     command_list = []
     command_count = 0
     for i in range(0, len(tokens)):
-        # print(f"i = {i}") #debug
-        # print(f"command = {tokens[i]}") #debug        
         if hasattr(request_object, tokens[i]): #check if token is a command in MySqliteREquest class
             command_list.append([])
             command_list[command_count].append(tokens[i])
@@ -41,7 +24,6 @@ def parse_prompt(user_input, request_object):
             command_list[command_count - 1].append(tokens[i])
         else:
             print("Error: unknown command or invalid arguments: " + user_input)
-    print(command_list) #debug
     return command_list
 
 def run_commands(command_list, request_object):
